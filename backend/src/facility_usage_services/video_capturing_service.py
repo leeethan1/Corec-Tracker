36cc4ca317f581012dd5b9466a0d11b8d18d44c8
Shen Wei Leong <shenwei0102@gmail.com>
2021-09-30 20:22:06 -0400

    restructured the folders

--- backend/src/facility_usage_services/video_capturing_service.py
+++ backend/src/facility_usage_services/video_capturing_service.py
@@ -9,7 +9,8 @@ while(cap.isOpened()):
     if ret == False:
         break
     
-    cv2.imwrite('../images/Frame'+str(i)+'.jpg', frame)
+    # takes one frame per sleep time and saving it to the image dir
+    cv2.imwrite('../../images/Frame'+str(i)+'.jpg', frame)
     i+=1
     # putting a minute for now just for testing purposes.
     time.sleep(60)
