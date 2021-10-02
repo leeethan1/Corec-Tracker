f03afab712f58480c97323d484abc15f13a6ab8f
Daniel Shi <daniel.shi@devz.ai>
2021-10-02 17:29:19 -0400

    added git ignore file

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from record_service import record_service
 from user_service import user_service
+import os
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 app.register_blueprint(record_service)
 app.register_blueprint(user_service)
-app.secret_key = 'SECRET_KEY'
+
+load_dotenv()
+app.secret_key = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
