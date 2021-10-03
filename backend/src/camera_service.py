e052126782af93719409b77a9f0c99d7baec1455
shenwei0102 <shenwei0102@gmail.com>
2021-10-02 23:06:24 -0400

    added service for counting people with unit tests

--- backend/src/camera_service.py
+++ backend/src/camera_service.py
@@ -1,6 +1,10 @@
 import cv2
 import time
 import datetime
+from flask import Blueprint, jsonify, request
+import record_service as rs
+import person_counter as pc
+import database_service
 
 # dictionary for mapping room name to the camera that's
 # scanning that room (only has one room for now)
@@ -8,22 +12,33 @@ room_to_camera = {
     'room 1': 0
 }
 
+camera_service = Blueprint('app_camera_service', __name__)
 
-def take_snapshot(room):
-    cap = cv2.VideoCapture(room_to_camera[room])
-    image_path = '../images/{}-{}.jpg'.format(room, str(datetime.datetime.now()))
-    while (cap.isOpened()):
-        ret, frame = cap.read()
+db = database_service.connect_to_database("database")
+records = db['records']
 
-        if ret == False:
-            break
 
-        # takes one frame per sleep time and saving it to the image dir
+@camera_service.route('/process-room', methods=['POST'])
+def process_room():
+    room = request.json['room']
+    try:
+        cap = cv2.VideoCapture(room_to_camera[room])
+        image_path = '../images/{}-{}.jpg'.format(room, str(datetime.datetime.now()))
+        while (cap.isOpened()):
+            ret, frame = cap.read()
 
-        cv2.imwrite(image_path, frame)
-        # putting a minute for now just for testing purposes.
-        time.sleep(60)
+            if ret == False:
+                # failed to capture image, return the last recorded occupancy
+                record = records.find({'room': room}).sort([('time', -1)]).limit(1)
+                return jsonify(record['occupancy'])
 
-    cap.release()
-    cv2.destroyAllWindows()
-    return image_path
+            cv2.imwrite(image_path, frame)
+
+        cap.release()
+        cv2.destroyAllWindows()
+        occupancy = pc.count_people_in_image(image_path)
+        return jsonify(rs.create_and_notify(room, occupancy))
+    except Exception as e:
+        # failed, return the last recorded occupancy
+        record = records.find({'room': room}).sort([('time', -1)]).limit(1)
+        return jsonify(record['occupancy'])
