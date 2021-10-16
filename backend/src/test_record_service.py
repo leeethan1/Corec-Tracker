1f628532e794a0254bde54657c477f2abcb23ece
danielshii <danielshi0516@gmail.com>
2021-10-15 20:00:18 -0400

    finished unit tests

--- backend/src/test_record_service.py
+++ backend/src/test_record_service.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 import database_service as ds
 import record_service as rs
@@ -7,22 +8,22 @@ records = db["records"]
 
 
 class TestRecordService(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        rs.create_record('room 1', 87, records)
-
     def testCreate(self):
+        rs.create_record('room 1', 87, records)
         record = records.find_one({'room': 'room 1'})
         assert record is not None
+        records.delete_many({})
 
     def testGet(self):
-        record_list = list(records.find({"hour": 17}))
+        for i in range(1, 11):
+            rs.create_record('room 1', i, records)
+        record_list = list(records.find({"hour": datetime.datetime.utcnow().hour}))
         occupancies = [record['occupancy'] for record in record_list]
         if not occupancies:
             average = 0
         else:
             average = sum(occupancies) / len(occupancies)
-        assert average == 87
+        assert average == 5.5
 
     @classmethod
     def tearDownClass(cls) -> None:
