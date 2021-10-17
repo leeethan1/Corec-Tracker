36300fc6cecfbb608d303b4f0dccc3a275e26332
ethanlee <lee3159@purdue.edu>
2021-10-17 17:00:37 -0400

    Fixed return val

--- backend/src/user_service.py
+++ backend/src/user_service.py
@@ -8,18 +8,18 @@ db = database_service.connect_to_database("database")
 users = db["users"]
 
 
-@user_service.route('/signup', methods=['POST', 'GET'])
+@user_service.route('/signup', methods=['POST'])
 def create_account():
+
     if "email" in session:
         return json.dumps("redirect")
     if request.method == 'POST':
         email = request.json["email"]
         password = request.json["password"]
         phone = request.json["phone"]
-        emailNotificationsOn = True;
+        emailNotificationsOn = True
         smsNotificationsOn = True
         notifications = {}
-
         # Uncomment this section when database is established
         user = users.find_one({"email": email})
         if user:
@@ -40,7 +40,7 @@ def create_account():
     return json.dumps("could not create account")
 
 
-@user_service.route('/login', methods=['post', 'get'])
+@user_service.route('/login', methods=['post'])
 def login():
     if "email" in session:
         return json.dumps("already logged in")
@@ -54,14 +54,39 @@ def login():
         if user:
             user_email = user['email']
             user_password = user['password']
+            # user_name = user['name']
 
             if bcrypt.checkpw(password.encode('utf-8'), user_password):
                 session["email"] = user_email
-                return "Logged in successfully", 200
+                return {'user': email}, 200
             else:
                 raise exceptions.AuthError
         else:
             raise exceptions.AuthError
+    raise exceptions.AuthError
+
+@user_service.route('/googlelogin', methods=['post'])
+def googleLogin():
+    if "email" in session:
+        return json.dumps("already logged in")
+
+    if request.method == "POST":
+        email = request.json["email"]
+
+        # Uncomment when db established
+        user = users.find_one({"email": email})
+        if not user:
+            emailNotificationsOn = True
+            smsNotificationsOn = True
+            notifications = {}
+            user_input = {'email': email,
+                'emailNotifications': emailNotificationsOn,
+                'smsNotifications': smsNotificationsOn,
+                'notifications': notifications,
+                'favoriteRooms': []}
+            users.insert_one(user_input)
+        return {'message': "Google log in success"}
+
     raise exceptions.AuthError
 
 
@@ -70,7 +95,7 @@ def logout():
     if 'email' not in session:
         return json.dumps("already logged out")
     session.pop('email', None)
-    return "Logged out successfully", 200
+    return {'message': "Logged out successfully"}, 200
 
 
 @user_service.route('/settings/email/update', methods=['PUT', 'GET'])
