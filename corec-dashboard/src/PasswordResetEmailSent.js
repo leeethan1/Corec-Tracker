2286103810ae908e1829dcc41283ae5f02bfbacb
danielshii <danielshi0516@gmail.com>
2021-10-18 19:05:57 -0400

    Revert "Merge pull request #30 from leeethan1/revert-29-daniel"
    This reverts commit 5b5fecb1061ef43bad7d05061e197cd5829f3af5, reversing
    changes made to 4fa48ff58448526e08f2286937afe3e891f38368.

---
+++ corec-dashboard/src/PasswordResetEmailSent.js
@@ -0,0 +1,25 @@
+import {React, useState} from "react";
+import { useHistory } from "react-router";
+import { Button } from "react-bootstrap";
+
+
+function PasswordResetEmailSent() {
+    const history = useHistory();
+
+    function redirectToLogin(res) {
+        history.push('/');
+    }
+
+    return (
+        <div>
+            <h1>
+                A link to reset your password was sent to your email.
+            </h1>
+            <Button onClick={redirectToLogin}>
+                Back to Login
+            </Button>
+        </div>
+    )
+}
+
+export default PasswordResetEmailSent
