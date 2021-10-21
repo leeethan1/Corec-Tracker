d409a1aed7bd1585c09578ae6ad0e4de291d81f9
Shen Wei Leong <shenwei0102@gmail.com>
2021-10-21 12:40:35 -0400

    fixing database connection

--- backend/src/camera_service.py
+++ backend/src/camera_service.py
@@ -3,29 +3,30 @@ import numpy as np
 import time
 import datetime
 from flask import Blueprint, jsonify, request
-# import record_service as rs
+import record_service as rs
 import person_counter as pc
 import database_service
 import person_counter as pc
 import record_service as rs
 
+
 # dictionary for mapping room name to the camera that's
 # scanning that room (only has one room for now)
 
-# room_to_camera = {
-#     'room 1': 0
-# }
+room_to_camera = {
+    'room 1': 0
+}
 
 
 camera_service = Blueprint('app_camera_service', __name__)
 
-# db = database_service.connect_to_database("database")
-# records = db['records']
+db = database_service.connect_to_database("database")
+records = db['records']
 
 
 @camera_service.route('/process-room', methods=['POST'])
 def process_room():
-    # room = request.json['room']
+    room = request.json['room']
     occupancy = 0
     try:
         cap = cv2.VideoCapture(0)
@@ -37,8 +38,8 @@ def process_room():
 
             if ret == False:
                 # failed to capture image, return the last recorded occupancy
-                # record = records.find({'room': room}).sort([('time', -1)]).limit(1)
-                # return jsonify(record[0]['occupancy'])
+                record = records.find({'room': room}).sort([('time', -1)]).limit(1)
+                return jsonify(record[0]['occupancy'])
                 break
 
             cv2.imwrite(image_path, frame)
