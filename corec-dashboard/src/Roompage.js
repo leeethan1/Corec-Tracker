e2f9061bdcb821fc45deeb637fa842ff498b3080
danielshii <danielshi0516@gmail.com>
2021-10-21 12:41:12 -0400

    playing around with different charts

--- corec-dashboard/src/Roompage.js
+++ corec-dashboard/src/Roompage.js
@@ -8,16 +8,18 @@ import {
 } from "react-router-dom";
 import {
   LineChart,
+  BarChart,
   XAxis,
   YAxis,
   Line,
   Tooltip,
   CartesianGrid,
   ResponsiveContainer,
+  Bar,
 } from "recharts";
 import { GoogleLogout } from "react-google-login";
 import Header from "./Header";
-import { Button } from "react-bootstrap";
+import { Button, Col, Row, Spinner } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import Tabs from "react-bootstrap/Tabs";
 import Tab from "react-bootstrap/Tab";
@@ -27,6 +29,7 @@ const cID =
 
 function Roompage() {
   const [occupancies, setOccupancies] = useState([]);
+  const [chartType, setChartType] = useState(0);
 
   //we probably need to have separate graphs for each room
   const [graphs, setGraphs] = useState([]);
@@ -118,7 +121,7 @@ function Roompage() {
       const averages = res.occupancies;
       //console.log(averages);
       setOccupancies(averages);
-      console.log(occupancies);
+      //console.log(occupancies);
     }
   }
 
@@ -131,61 +134,78 @@ function Roompage() {
     return "Room 1";
   }
 
-  function renderChart() {
-    return (
-      <div className="center">
-        {/* <Header /> */}
-        <LineChart
-          width={1000}
-          height={500}
-          data={graphData}
+  function renderChart(chart) {
+    switch (chart) {
+      case 0:
+        return (
+          <LineChart
+            width={1000}
+            height={400}
+            data={graphData}
 
-          // margin={{
-          //   top: 5,
-          //   right: 30,
-          //   left: 20,
-          //   bottom: 5,
-          // }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="time" />
-          <YAxis />
-          <Tooltip />
-          <Line
-            type="monotone"
-            dataKey="ct"
-            stroke="#8884d8"
-            activeDot={{ r: 5 }}
-          />
-        </LineChart>
-        {/* <Button onClick={handleGetOccupancies}>Refresh</Button> */}
-      </div>
-    );
+            // margin={{
+            //   top: 5,
+            //   right: 30,
+            //   left: 20,
+            //   bottom: 5,
+            // }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="time" />
+            <YAxis />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="ct"
+              stroke="#8884d8"
+              activeDot={{ r: 5 }}
+            />
+          </LineChart>
+        );
+        break;
+      case 1:
+        return (
+          <BarChart width={1000} height={400} data={graphData}>
+            <XAxis dataKey="time" fill="#000000" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="ct"></Bar>
+          </BarChart>
+        );
+      default:
+        return <h1>NA</h1>;
+    }
   }
 
   //for some reason the chart isn't rendering when i put it in the tab
   return (
-    <div className="Roompage">
+    <div className="vertical">
       <Header />
-      <h1
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        Room {roomNumber}
-      </h1>
-      <h2
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        Live Occupancy: <b>56</b>
+      <h1 className="center">Room {roomNumber}</h1>
+      <h2 className="center">
+        <Col>
+          Live Occupancy: <b>56</b>{" "}
+          <Spinner variant="danger" animation="grow" size="sm" />
+        </Col>
       </h2>
-      {renderChart()}
+      <span className="center">
+        <span className="vertical">
+          <Tabs
+            id="chart tabs"
+            activeKey={0}
+            onSelect={(k) => setChartType(k)}
+          >
+            {[0,1].map((element, index) => (
+              <Tab eventKey={element} title={index}>
+                {renderChart(chartType)}
+                {/* <h1>{element}</h1> */}
+              </Tab>
+            ))}
+          </Tabs>
+        </span>
+        {/* {renderChart(0)}
+        {renderChart(1)} */}
+      </span>
 
       {/* <GoogleLogout
         clientId={cID}
