6230f88709e544c160c637cbb5f102c3d345597d
Shen Wei Leong <shenwei0102@gmail.com>
2021-11-21 22:31:01 -0500

    Fully implemented mobile

--- corec-dashboard/src/Login.js
+++ corec-dashboard/src/Login.js
@@ -3,13 +3,13 @@ import { useHistory } from "react-router-dom";
 import { Form, FormCheck, InputGroup } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import GoogleLogin from "react-google-login";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/fontawesome-free-solid'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/fontawesome-free-solid";
+import axios from "axios";
 
 const cID =
   "608867787381-cvgulq19nomsanr5b3ho6i2kr1ikocbs.apps.googleusercontent.com";
 const facebookID = "294054042557801";
-
 
 var rememberUser = false;
 export { rememberUser };
@@ -27,8 +27,37 @@ function Login({ setLogIn }) {
     return email.length > 0 && password.length > 0;
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
+  async function handleChatLogin(e, secret) {
+    e.preventDefault();
+    const authHeader = {
+      "Private-Key": "b35df4a0-b81b-45d0-b331-0b077b14d0bc",
+    };
+    const authObject = {
+      "Project-ID": "9e45fcff-6309-40db-b521-4ef91549ccd2",
+      "User-Name": email,
+      "User-Secret": password,
+    };
+    try {
+      await axios.put(
+        "https://api.chatengine.io/users",
+        {
+          username: email,
+          secret: secret,
+        },
+        {
+          headers: authHeader,
+        }
+      );
+      sessionStorage.setItem("username", email);
+      //sessionStorage.setItem("password", secret);
+      if (remember) {
+        localStorage.setItem("username", email);
+        //localStorage.setItem("password", secret);
+      }
+    } catch (error) {
+      console.log(error);
+      //setLoginFail(true);
+    }
   }
 
   async function handleGetLogin() {
@@ -55,7 +84,8 @@ function Login({ setLogIn }) {
     handleGetLogin();
   }, []);
 
-  async function handleLogin() {
+  async function handleLogin(e) {
+    e.preventDefault();
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -80,8 +110,7 @@ function Login({ setLogIn }) {
         //sessionStorage.setItem("access", localStorage.getItem("access"));
         //sessionStorage.setItem("refresh", localStorage.getItem("refresh"));
       }
-      localStorage.setItem("access", tokens.access_token);
-      localStorage.setItem("refresh", tokens.refresh_token);
+      handleChatLogin(e, tokens.access_token);
       history.push("/dashboard", { user: "test" });
     } else {
       setLoginFail(true);
@@ -92,11 +121,7 @@ function Login({ setLogIn }) {
 
   function formFailure() {
     if (loginFail) {
-      return (
-        <div>
-          <b style={{ color: "red" }}>Email or password is incorrect</b>
-        </div>
-      );
+      return <div style={{ color: "red" }}>Email or password is incorrect</div>;
     }
   }
 
@@ -141,12 +166,12 @@ function Login({ setLogIn }) {
   return (
     <div>
       <div id="Login-Panel">
-      <h1>Login</h1>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group size="lg" controlId="email" className="mb-3">
-          <InputGroup>
+        <h1>Login</h1>
+        <Form onSubmit={handleLogin}>
+          <Form.Group size="lg" controlId="email" className="mb-3">
+            <InputGroup>
               <InputGroup.Text>
-              <FontAwesomeIcon icon="envelope" />
+                <FontAwesomeIcon icon="envelope" />
               </InputGroup.Text>
               <Form.Control
                 autoFocus
@@ -155,77 +180,79 @@ function Login({ setLogIn }) {
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
               />
-          </InputGroup>
-        </Form.Group>
-        <Form.Group size="lg" controlId="password" className="mb-3">
-        <InputGroup>
+            </InputGroup>
+          </Form.Group>
+          <Form.Group size="lg" controlId="password" className="mb-3">
+            <InputGroup>
               <InputGroup.Text>
-              <FontAwesomeIcon icon="key" />
+                <FontAwesomeIcon icon="key" />
               </InputGroup.Text>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {formFailure()}
-          </InputGroup>
-        </Form.Group>
-        <div id="Remember-Forgot">
-          <FormCheck
-            label={<p>Remember Me</p>}
-            onChange={() => setRemember(!remember)}
-            checked={remember}
-          />
-          <a id="Forgot" href="/forgot-password">Forgot Password</a>
-        </div>
-        <div id="Login-Aree">
-        <Button
-          id="Login-Button"
-          block
-          size="lg"
-          type="submit"
-          onClick={handleLogin}
-          disabled={!validateForm()}
-        >
-          Log In
-        </Button>
-        </div>
-        <a href="/signup" id="Signup-Link">
-          Don't have an account? Sign up
-        </a>
-        <div id="Other-Options">
-        <Button
-          block
-          size="lg"
-          type="submit"
-          variant="secondary"
-          onClick={(e) => {
-            localStorage.removeItem("access");
-            localStorage.removeItem("refresh");
-            sessionStorage.removeItem("access");
-            sessionStorage.removeItem("refresh");
-            history.push("/dashboard");
-          }}
-        >
-          Continue as Guest
-        </Button>
-        <GoogleLogin
-          // render={(renderProps) => {
-          //   return (
-          //   <Button onClick={renderProps.onClick} disabled={renderProps.disabled}>
-          //     <FontAwesomeIcon icon="google" />
-          //   </Button>)
-          // }}
-          theme="dark"
-          clientId={cID}
-          buttonText="Log in with Google"
-          onSuccess={handleGoogleSuccess}
-          onFailure={handleGoogleFailure}
-          cookiePolicy={"single_host_origin"}
-        />
-        </div>
-        {/* <FacebookLogin
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {formFailure()}
+            </InputGroup>
+          </Form.Group>
+          <div id="Remember-Forgot">
+            <FormCheck
+              label={<p>Remember Me</p>}
+              onChange={() => setRemember(!remember)}
+              checked={remember}
+            />
+            <a id="Forgot" href="/forgot-password">
+              Forgot Password
+            </a>
+          </div>
+          <div id="Login-Aree">
+            <Button
+              id="Login-Button"
+              block
+              size="lg"
+              type="submit"
+              onClick={handleLogin}
+              disabled={!validateForm()}
+            >
+              Log In
+            </Button>
+          </div>
+          <a href="/signup" id="Signup-Link">
+            Don't have an account? Sign up
+          </a>
+          <div id="Other-Options">
+            <Button
+              block
+              size="md"
+              type="submit"
+              variant="secondary"
+              onClick={(e) => {
+                localStorage.removeItem("access");
+                localStorage.removeItem("refresh");
+                sessionStorage.removeItem("access");
+                sessionStorage.removeItem("refresh");
+                history.push("/dashboard");
+              }}
+            >
+              Continue as Guest
+            </Button>
+            <GoogleLogin
+              // render={(renderProps) => {
+              //   return (
+              //   <Button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+              //     <FontAwesomeIcon icon="google" />
+              //   </Button>)
+              // }}
+              theme="dark"
+              clientId={cID}
+              buttonText="Log in with Google"
+              onSuccess={handleGoogleSuccess}
+              onFailure={handleGoogleFailure}
+              cookiePolicy={"single_host_origin"}
+            />
+          </div>
+          {/* <FacebookLogin
           appId={facebookID}
           autoLoad={true}
           fields="name,email,picture"
@@ -233,7 +260,7 @@ function Login({ setLogIn }) {
           cssClass="my-facebook-button-class"
           icon="fa-facebook"
         /> */}
-      </Form>
+        </Form>
       </div>
     </div>
   );
