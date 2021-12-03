c22cc9474b4bc104bb4aea593068b31f46b14781
danielshii <danielshi0516@gmail.com>
2021-12-03 13:06:20 -0500

    fixed person counter test

--- backend/src/test_person_counter.py
+++ backend/src/test_person_counter.py
@@ -4,5 +4,5 @@ import unittest
 
 class TestPersonCounter(unittest.TestCase):
     def testCount(self):
-        assert pc.count_people_in_image('../images/test/2021-10-15.jpg') == 1
+        assert pc.count_people_in_image('../images/test/Frame1.jpg') == 1
         #assert pc.count_people_in_image('../images/test/input_image3.png') == 3
